1821dde4e431e8c9534e12bcf40484b133edecf8
jenny <yenc828@newschool.edu>
2025-04-02 16:44:10 -0400

    create json

--- main.js
+++ main.js
@@ -2,6 +2,30 @@ window.stateCallback = () => {
 	console.log('Something has changed!')
 }
 
+mainArray = [
+    {
+        "name":"Toast",
+        "image": " assets/toast.png ",
+        "calories": 74
+    },
+    {
+        "name":"Bagel",
+        "image": " assets/bagel.png ",
+        "calories": 277
+    },
+    {
+        "name":"Croissant",
+        "image": " assets/croissant.png ",
+        "calories": 120
+    },
+    {
+        "name":"Pancake",
+        "image": " assets/pancake.png ",
+        "calories": 120
+    }
+]
+
+
 //I wnat to show images when checkboxed are checked
 //I found this: https://jsfiddle.net/vaWtY/
 
